d3e941889af6124319e6f30b301ea3a9d02abb02
nefise <2019555034@ogr.cu.edu.tr>
2022-09-01 20:29:13 +0300

    react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import { createRoot } from 'react-dom/client';
+import "./index.css"
+//import ReactDom from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Person=({img,name,job,children})=>{
+   const url=`https://randomuser.me/api/portraits/thumb/men/${img}.jpg`
+  return(
+    <article className='person'>
+      <img src={url} alt="" />
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+      {children}
+    </article>
+  );
+}
+
+const PersonList=()=>{
+  return(
+    <section className='personList'>
+      <Person img="55" name="ahmet" job="civil servant"/>
+      <Person img="75" name="nefise" job="web dev">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, enim in exercitationem harum fuga sequi et, velit autem omnis cupiditate atque molestiae odio possimus deleniti vero natus unde aspernatur animi.</p>
+      </Person>
+      <Person img="15" name="ömer" job="student"/>
+    </section>
+  );
+}
+const container=document.getElementById("root");
+const root=createRoot(container);
+root.render(<PersonList/>);
+//ReactDom.render(<PersonList/>,document.getElementById("root"));
